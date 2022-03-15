4096af3a44ce389d83cdd56763c9d15e99e959d9
lw-xjtu <82857569+lw-xjtu@users.noreply.github.com>
2022-03-15 17:00:34 +0800

    Add files via upload

--- CodeCraft-2022.py
+++ CodeCraft-2022.py
@@ -1,16 +1,17 @@
 import numpy as np
 from collections import defaultdict
+import math
 # 导包
 import configparser
 config = configparser.ConfigParser() # 类实例化
 # 定义参数文件路径
-path = r'D:\华为比赛\SDK\SDK_python\data\config.ini'
+path = r'./data/config.ini'
 config.read(path)
 #导入参数配置
 qos_constraint = int(config.get('config','qos_constraint'))
 # print(qos_constraint)
 #客户
-with open('D:\华为比赛\SDK\SDK_python\data\demand.csv','rb') as f:
+with open('./data/demand.csv','rb') as f:
     data = np.loadtxt(f, str, delimiter=",")
     #客户名称
     kehu = data[0][1:]
@@ -21,7 +22,7 @@ with open('D:\华为比赛\SDK\SDK_python\data\demand.csv','rb') as f:
     for i in range(T):
         for j in range(kehu_number):
             D[kehu[j]].append(int(data[i+1][j+1]))
-with open('D:\华为比赛\SDK\SDK_python\data\site_bandwidth.csv','rb') as s:
+with open('./data/site_bandwidth.csv','rb') as s:
     data1 = np.loadtxt(s,str,delimiter=",")
     jiedian_number = len(data1[1:])
     #边缘节点名称
@@ -36,24 +37,29 @@ def addtwodimdict(thedict, key_a, key_b, val):
     thedict[key_a].update({key_b: val})
   else:
     thedict.update({key_a:{key_b: val}})
-with open('D:\华为比赛\SDK\SDK_python\data\qos.csv','rb') as q:
+with open('./data/qos.csv','rb') as q:
     data2 = np.loadtxt(q,str,delimiter=',')
     # Q[边缘节点][客户节点]=qos
-    #X[客户节点][边缘节点]为分配带宽，先初始化为0
     Q = defaultdict(int)
-    X = defaultdict(int)
     for i in range(len(data2)-1):
         for j in  range(len(data2[0])-1):
             # Q[data[0][i+1]][data[j+1][0]] = int(data[i+1][j+1])
             addtwodimdict(Q,data2[i+1][0],data2[0][j+1],int(data2[i+1][j+1]))
-            addtwodimdict(X,data2[0][j+1],data2[i+1][0],0)
+
+solution = open('./output/solution.txt','w')
 #初始化j节点t时刻总带宽
 W = defaultdict(list)
 for j in range(jiedian_number):
     for t in range(T):
         W[jiedian[j]].append(0)
 for t in range(T):
+    # X[客户节点][边缘节点]为分配带宽，先初始化为0
+    X = defaultdict(int)
+    for i in range(len(data2)-1):
+        for j in  range(len(data2[0])-1):
+            addtwodimdict(X, data2[0][j + 1], data2[i + 1][0], 0)
     for i in range(kehu_number):
+        print('{}:'.format(kehu[i]), file=solution,end='')
         for j in range(jiedian_number):
             #客户带宽分配完毕
             if D[kehu[i]][t] == 0:
@@ -71,16 +77,19 @@ for t in range(T):
                         D[kehu[i]][t] = 0
                     else:
                         X[kehu[i]][jiedian[j]] = rest
-                        D[kehu[i]][t] -= rest
+                        D[kehu[i]][t] -= X[kehu[i]][jiedian[j]]
                     W[jiedian[j]][t] += X[kehu[i]][jiedian[j]]
                 #分配总带宽达到节点上限
                 else:
                     continue
+                if X[kehu[i]][jiedian[j]] != 0:
+                    print('<{},{}>'.format(jiedian[j],X[kehu[i]][jiedian[j]]),file=solution,end='')
+        print('\r',file=solution)
 s = 0
 for j in range(jiedian_number):
     #总带宽排序
     W[jiedian[j]].sort()
-    s += W[jiedian[j]][int(T*0.95)]
+    s += W[jiedian[j]][math.ceil(T*0.95)]
 print(s)
 
 
