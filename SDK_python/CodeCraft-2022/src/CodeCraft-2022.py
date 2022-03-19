f0f0964f9583c11d6985f164bd2e615e5ab7b989
lw-xjtu <82857569+lw-xjtu@users.noreply.github.com>
2022-03-19 16:39:54 +0800

    Add files via upload

--- SDK_python/CodeCraft-2022/src/CodeCraft-2022.py
+++ SDK_python/CodeCraft-2022/src/CodeCraft-2022.py
@@ -3,13 +3,12 @@ Author: fujiawei0724
 Date: 2022-03-18 09:27:23
 LastEditors: fujiawei0724
 LastEditTime: 2022-03-19 14:08:08
-Description: 
+Description:
 '''
 import numpy as np
 import configparser
 import copy
 from collections import defaultdict
-
 
 
 class Tools:
@@ -21,60 +20,60 @@ class Tools:
         available_edge_nodes_info = dict()
         for edge_node in available_edge_nodes:
             available_edge_nodes_info[edge_node] = connected_number[edge_node]
-        
+
         # Sort
-        available_edge_nodes_info = sorted(available_edge_nodes_info.items(), key=lambda x:x[1], reverse=True)
+        available_edge_nodes_info = sorted(available_edge_nodes_info.items(), key=lambda x: x[1], reverse=True)
 
         return list(dict(available_edge_nodes_info).keys())
-
 
     @staticmethod
     def addtwodimdict(thedict, key_a, key_b, val):
         if key_a in thedict:
             thedict[key_a].update({key_b: val})
         else:
-            thedict.update({key_a:{key_b: val}})
+            thedict.update({key_a: {key_b: val}})
+
 
 if __name__ == '__main__':
-    config = configparser.ConfigParser() # 类实例化
+    config = configparser.ConfigParser()  # 类实例化
     # 定义参数文件路径
     path = './data/config.ini'
     config.read(path)
     # 导入参数配置
-    qos_constraint = int(config.get('config','qos_constraint'))
+    qos_constraint = int(config.get('config', 'qos_constraint'))
     # print(qos_constraint)
     # qos_constraint = 400
-    #客户
-    with open('./data/demand.csv','rb') as f:
+    # 客户
+    with open('./data/demand.csv', 'rb') as f:
         data = np.loadtxt(f, str, delimiter=",")
-        #客户名称
+        # 客户名称
         kehu = data[0][1:]
         kehu_number = len(kehu)
         T = len(data[1:])
         D = defaultdict(list)
-        #客户宽带需求
+        # 客户宽带需求
         for i in range(T):
             for j in range(kehu_number):
-                D[kehu[j]].append(int(data[i+1][j+1]))
-    with open('./data/site_bandwidth.csv','rb') as s:
-        data1 = np.loadtxt(s,str,delimiter=",")
+                D[kehu[j]].append(int(data[i + 1][j + 1]))
+    with open('./data/site_bandwidth.csv', 'rb') as s:
+        data1 = np.loadtxt(s, str, delimiter=",")
         jiedian_number = len(data1[1:])
-        #边缘节点名称
+        # 边缘节点名称
         jiedian = []
-        #边缘节点带宽上限
+        # 边缘节点带宽上限
         C = defaultdict()
         for i in range(jiedian_number):
-            jiedian.append(data1[i+1][0])
-            C[data1[i+1][0]] = int(data1[i+1][1])
-    with open('./data/qos.csv','rb') as q:
-        data2 = np.loadtxt(q,str,delimiter=',')
+            jiedian.append(data1[i + 1][0])
+            C[data1[i + 1][0]] = int(data1[i + 1][1])
+    with open('./data/qos.csv', 'rb') as q:
+        data2 = np.loadtxt(q, str, delimiter=',')
         # Q[边缘节点][客户节点]=qos
         Q = defaultdict(int)
-        for i in range(len(data2)-1):
-            for j in  range(len(data2[0])-1):
+        for i in range(len(data2) - 1):
+            for j in range(len(data2[0]) - 1):
                 # Q[data[0][i+1]][data[j+1][0]] = int(data[i+1][j+1])
-                Tools.addtwodimdict(Q,data2[i+1][0],data2[0][j+1],int(data2[i+1][j+1]))
-    
+                Tools.addtwodimdict(Q, data2[i + 1][0], data2[0][j + 1], int(data2[i + 1][j + 1]))
+
     # print(T)
     # print(D)
     # print(Q)
@@ -82,37 +81,37 @@ if __name__ == '__main__':
     # print(qos_constraint)
 
     # Calculate the maximum fully loaded numbers
-    maximum_fully_loaded_num = int(T * 0.05) -1
+    maximum_fully_loaded_num = int(T * 0.05) - 1
 
     # Record the current fully loaded numbers of each edge node
     fully_loaded_numbers = defaultdict(int)
     for edge_node in jiedian:
         fully_loaded_numbers[edge_node] = 0
 
-    # Record the connected clients number of a edge node and the available edge nodes for each client 
+    # Record the connected clients number of a edge node and the available edge nodes for each client
     connected_numer = defaultdict(int)
-    connection = defaultdict(list)
+    count = defaultdict(int)
     for edge_node in jiedian:
         for client in kehu:
-            if Q[edge_node][client] <= qos_constraint:
+            if Q[edge_node][client] < qos_constraint:
                 connected_numer[edge_node] += 1
-                connection[client].append(edge_node)
+                count[client] += 1
     # print(available_edge_nodes)
 
     # allocation_record = dict()
 
-    solution = open('./output/solution.txt','w')
+    solution = open('./output/solution.txt', 'w')
 
     W = defaultdict(list)
     for j in range(jiedian_number):
         for t in range(T):
             W[jiedian[j]].append(0)
-    count = defaultdict(int)
-    for i in range(kehu_number):
-        for j in range(jiedian_number):
-            if Q[jiedian[j]][kehu[i]] < qos_constraint:
-                count[kehu[i]] += 1
-    
+    # count = defaultdict(int)
+    # for i in range(kehu_number):
+    #     for j in range(jiedian_number):
+    #         if Q[jiedian[j]][kehu[i]] < qos_constraint:
+    #             count[kehu[i]] += 1
+
     for t in range(T):
 
         # Record the allocation detail at the current timestamp
@@ -127,12 +126,13 @@ if __name__ == '__main__':
         for edge_node in jiedian:
             if fully_loaded_numbers[edge_node] < maximum_fully_loaded_num:
                 cur_available_edge_nodes.append(edge_node)
-        if len(cur_available_edge_nodes) == 0:
-            print('No available edge nodes.')
+        # if len(cur_available_edge_nodes) == 0:
+        #     print('No available edge nodes.')
 
         # Sort the available edge nodes
         sel_edge_nodes = Tools.select_edge_nodes(cur_available_edge_nodes, connected_numer)
-            
+        # print(sel_edge_nodes)
+
         for sel_edge_node in sel_edge_nodes:
 
             # Backup infomration
@@ -157,20 +157,17 @@ if __name__ == '__main__':
                         sel_edge_node_avail_bandwidth -= cur_client_demand
                         X[client][sel_edge_node] += cur_client_demand
                         W[sel_edge_node][t] += cur_client_demand
-            
+
             if W[sel_edge_node][t] == C[sel_edge_node]:
                 # Fully loaded
                 fully_loaded_numbers[sel_edge_node] += 1
             else:
-                # Not fully loaded, restore allocation information 
+                # Not fully loaded, restore allocation information
                 D = D_backup
                 X = X_backup
                 W = W_backup
                 break
 
-
-
-        
         # Allocate remain demanding
         for i in range(kehu_number):
             print('{}:'.format(kehu[i]), file=solution, end='')
@@ -241,29 +238,12 @@ if __name__ == '__main__':
             # print(res)
     s = 0
     for j in range(jiedian_number):
-        #总带宽排序
+        # 总带宽排序
         W[jiedian[j]].sort()
-        s += W[jiedian[j]][int(T*0.95)]
+        s += W[jiedian[j]][int(T * 0.95)]
     print(s)
     # print(W)
     # print(D)
     # print(X)
- 
 
-        
-
-            
-
-        
-
-        
-            
-            
-        
-
-    
-    
-
-    
-    
 
